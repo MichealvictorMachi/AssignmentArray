62ed40f893746ca51644057f115f81c950fe54de
Victor Micheal <michealvictor175@gmail.com>
2022-08-05 09:07:24 +0100

    Add project files.

---
+++ AssignmentArray/ArraySort.cs
@@ -0,0 +1,30 @@
+﻿namespace AssignmentArray
+{
+    internal class DescSortings
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int[] array = new int[11] { 23, 43, 12, 87, 56, 90, 65, 18, 49, 9, 32 };
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                for (int j = 0; j <= array.Length - 2; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        x = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = x;
+                    }
+                }
+
+            }
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
